aedad7e52a1126c828108cbebc8ae5142d104cd8
charliec <yingyue@gmail.com>
2019-04-14 19:25:45 +0800

    ->49

---
+++ projects/ex49/ex49/lexicon.py
@@ -0,0 +1,32 @@
+def scan(text):
+    words = text.split()
+    result = []
+
+    directions = ('north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', 'back')
+    verbs = ('go', 'stop', 'kill', 'eat')
+    stops = ('the', 'in', 'of', 'from', 'at', 'it')
+    nouns = ('door', 'bear', 'princess', 'cabinet')
+
+    for word in words:
+        if word in directions:
+            word_append = 'direction', word
+        elif word in verbs:
+            word_append = 'verb', word
+        elif word in stops:
+            word_append = 'stop', word
+        elif word in nouns:
+            word_append = 'noun', word
+        elif convert_number(word):
+            word_append = 'number', word
+        else:
+            word_append = 'error', word
+
+        result.append(word_append)
+
+    return result
+
+def convert_number(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
